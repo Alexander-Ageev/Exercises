c877d76b7908e87bc52f263368c883287e852bca
Alexander Ageev <alexandrageev87@gmail.com>
2021-08-16 23:32:51 +0300

    Ex. 6

---
+++ Pure code/6.py
@@ -0,0 +1,12 @@
+Выполните 12 улучшений имён функций/методов в вашем коде в формате "было - стало - ваш комментарий", и выложите на гитхаб.
+square - calc_square # ООП. Полиморфизм
+perimeter - calc_perimeter # ООП. Полиморфизм
+change - change_parameters # ООП. Методы
+check - check_time / check_date # ООП. Методы
+display - display_message # ООП. Методы
+_clear - _remove_branch_and_adjacent # TreeOfLife
+remove_branches - remove_old_branches # TreeOfLife
+report - generate_report # TreeOfLife
+new_sprig - create_sprig # TreeOfLife
+growth - grow # TreeOfLife
+# Больше улучшать нечего (остальное либо совсем однотипное, либо достаточно хорошо соответствует коду)
