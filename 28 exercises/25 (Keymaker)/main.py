13633bca99e5bc1dadd7d503a6b573b829c29f79
Alexander Ageev <alexandrageev87@gmail.com>
2021-08-22 18:06:33 +0300

    Ex. 8 refactoring

--- 28 exercises/25 (Keymaker)/main.py
+++ 28 exercises/25 (Keymaker)/main.py
@@ -1,8 +1,11 @@
+CLOSE_DOOR = '0'
+OPEN_DOOR = '1'
+
 def Keymaker(doors_count):
-    state_of_doors = ['0' for i in range(doors_count)]        
+    state_of_doors = [CLOSE_DOOR for i in range(doors_count)]        
     door_index = 0
     while (door_index+1) ** 2  <= doors_count:
-        state_of_doors[(door_index+1) ** 2 - 1] = '1'
+        state_of_doors[(door_index+1) ** 2 - 1] = OPEN_DOOR
         door_index += 1
     result_sate_of_doors = ''.join(state_of_doors)
     return result_sate_of_doors
