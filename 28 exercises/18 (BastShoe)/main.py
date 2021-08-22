0883fe19e98fd4f21c3d94702739aabd324bb02f
Alexander Ageev <alexandrageev87@gmail.com>
2021-08-22 17:43:02 +0300

    Ex. 8 refactoring

--- 28 exercises/18 (BastShoe)/main.py
+++ 28 exercises/18 (BastShoe)/main.py
@@ -1,8 +1,8 @@
-ADD_COMAND = '1'
-DELETE_COMAND = '2'
-GET_CHAR_COMAND = '3'
-UNDO_COMAND = '4'
-REDO_COMAND = '5'
+ADD_COMMAND = '1'
+DELETE_COMMAND = '2'
+GET_CHAR_COMMAND = '3'
+UNDO_COMMAND = '4'
+REDO_COMMAND = '5'
 
 index = 0
 Task_Queue = [] # list of (command, data)
@@ -10,9 +10,9 @@ Task_Queue = [] # list of (command, data)
 def queue_processing():
     result = ''
     for i in range(index):
-        if Task_Queue[i][0] == ADD_COMAND:
+        if Task_Queue[i][0] == ADD_COMMAND:
             result += Task_Queue[i][1]
-        elif Task_Queue[i][0] == DELETE_COMAND:
+        elif Task_Queue[i][0] == DELETE_COMMAND:
             if len(result) > int(Task_Queue[i][1]):
                 result = result [0: len(result) - int(Task_Queue[i][1])]
             else:
@@ -23,29 +23,29 @@ def BastShoe(command):
     global index, Task_Queue
     key = command[0]
     data = command[2:]
-    if key == ADD_COMAND and index == len(Task_Queue): # add operation, no undo operations
+    if key == ADD_COMMAND and index == len(Task_Queue): # add operation, no undo operations
         Task_Queue.append( (key, data) )
         index = len(Task_Queue)
-    elif key == DELETE_COMAND and index == len(Task_Queue): # delete operation, no undo
+    elif key == DELETE_COMMAND and index == len(Task_Queue): # delete operation, no undo
         Task_Queue.append( (key, data) )
         index = len(Task_Queue)
-    elif index < len(Task_Queue) and key in [ADD_COMAND, DELETE_COMAND]: # undo operations apply
-        Task_Queue = [ (ADD_COMAND, queue_processing()) ]
+    elif index < len(Task_Queue) and key in [ADD_COMMAND, DELETE_COMMAND]: # undo operations apply
+        Task_Queue = [ (ADD_COMMAND, queue_processing()) ]
         Task_Queue.append( (key, data) )
         index = len(Task_Queue)
-    elif key == GET_CHAR_COMAND: # get text[index]
+    elif key == GET_CHAR_COMMAND: # get text[index]
         result = queue_processing()    
         try:
             result = result[int(data)]
         except:
             result = ''
         return result
-    elif key == UNDO_COMAND:
+    elif key == UNDO_COMMAND:
         if index > 1:
             index -= 1
         else:
             index = 1
-    elif key == REDO_COMAND:
+    elif key == REDO_COMMAND:
         if index < len(Task_Queue):
             index += 1
         else:
