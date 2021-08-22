b5903589f3eff8390b342153beee3fe780272158
Alexander Ageev <alexandrageev87@gmail.com>
2021-08-22 17:31:15 +0300

    Ex. 8 refactoring

--- 28 exercises/18 (BastShoe)/main.py
+++ 28 exercises/18 (BastShoe)/main.py
@@ -1,13 +1,18 @@
+ADD_COMAND = '1'
+DELETE_COMAND = '2'
+GET_CHAR_COMAND = '3'
+UNDO_COMAND = '4'
+REDO_COMAND = '5'
+
 index = 0
 Task_Queue = [] # list of (command, data)
-
 
 def queue_processing():
     result = ''
     for i in range(index):
-        if Task_Queue[i][0] == '1':
+        if Task_Queue[i][0] == ADD_COMAND:
             result += Task_Queue[i][1]
-        elif Task_Queue[i][0] == '2':
+        elif Task_Queue[i][0] == DELETE_COMAND:
             if len(result) > int(Task_Queue[i][1]):
                 result = result [0: len(result) - int(Task_Queue[i][1])]
             else:
@@ -18,29 +23,29 @@ def BastShoe(command):
     global index, Task_Queue
     key = command[0]
     data = command[2:]
-    if key == '1' and index == len(Task_Queue): # add operation, no undo operations
+    if key == ADD_COMAND and index == len(Task_Queue): # add operation, no undo operations
         Task_Queue.append( (key, data) )
         index = len(Task_Queue)
-    elif key == '2' and index == len(Task_Queue): # delete operation, no undo
+    elif key == DELETE_COMAND and index == len(Task_Queue): # delete operation, no undo
         Task_Queue.append( (key, data) )
         index = len(Task_Queue)
-    elif index < len(Task_Queue) and key in ['1', '2']: # undo operations apply
-        Task_Queue = [ ('1', queue_processing()) ]
+    elif index < len(Task_Queue) and key in [ADD_COMAND, DELETE_COMAND]: # undo operations apply
+        Task_Queue = [ (ADD_COMAND, queue_processing()) ]
         Task_Queue.append( (key, data) )
         index = len(Task_Queue)
-    elif key == '3': # get text[index]
+    elif key == GET_CHAR_COMAND: # get text[index]
         result = queue_processing()    
         try:
             result = result[int(data)]
         except:
             result = ''
         return result
-    elif key == '4': # undo
+    elif key == UNDO_COMAND:
         if index > 1:
             index -= 1
         else:
             index = 1
-    elif key == '5': #redo
+    elif key == REDO_COMAND:
         if index < len(Task_Queue):
             index += 1
         else:
