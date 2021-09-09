d9c7192c3f6a4df5bacdfa9e0b0d4c1c98fb7e1b
Alexander-Ageev <alexadrageev87@gmai.com>
2021-09-09 23:39:08 +0300

    Ex. 14

---
+++ Pure code/14.py
@@ -0,0 +1,51 @@
+DIFFERENT_YEARS = 2 # Variants of the differents growth's ways
+
+# Create matrix H*W, where 0 - none, n - age of branches
+    def __init__ (self, H, W, tree):
+
+# Комментарии помогают проще понять, какой именно случай развирается в условии
+if len(set(diff_symbols_count)) == 1:# string [n, n, ... , n] type ('abc')
+            input_string_valid = True
+        elif min(counter(diff_symbols_count)) == 1 and exclusion_symbol_number - base_symbol_number == 1:# string [1, n, n, ... , n] type ('abbccdd')
+            input_string_valid = True
+        elif min(counter(diff_symbols_count)) == 1 and exclusion_symbol_number == 1:# string [n, n, ... , n, n+1] type ('abcdd')
+
+
+if key == ADD_COMMAND and index == len(Task_Queue): # add operation, no undo operations
+        Task_Queue.append( (key, data) )
+        index = len(Task_Queue)
+    elif key == DELETE_COMMAND and index == len(Task_Queue): # delete operation, no undo
+        Task_Queue.append( (key, data) )
+        index = len(Task_Queue)
+    elif index < len(Task_Queue) and key in [ADD_COMMAND, DELETE_COMMAND]: # undo operations apply
+
+# Комментарий поясняет дополнительные неочевидные условия 
+# по условию наличие символов A-F во входном массиве не предусмотрено
+def hexacode(number):
+    result = 0
+    hexo = str(number)[::-1]
+    for i in range(len(hexo)):
+        result += int(hexo[i]) * 16**i
+    return result   
+
+FOUNDATION = 4 #Количество цифр, на которое разбивается число
+
+def TheRabbitsFoot(source_string, encode_mode): # encode = true: зашифровать; encode = false: расшировать
+
+
+# последняя строка сделала комментарий не нужным
+def merge_items(item_data_array):   # data_array: [name count]
+    db = {} # {name : count}
+    for i in range(len(item_data_array)):
+        item_data = item_data_array[i].split()
+        item_name, item_count = item_data
+
+# Вторая строка улучшила читаемость, комментарий стал не нужен
+ sequence_info = separator(diff_symbols_count)# [base_seq, exclusion]
+        base_symbol_number, exclusion_symbol_number = sequence_info
+
+# Имя константы говорит само за себя
+REMOVE_BRANCH_TEMPLATE = [(-1, 0), (0, 1), (1, 0), (0, -1)] # Map of the clear branches
+
+# Ненужный комментарий
+len_part = 2 # length current partition
