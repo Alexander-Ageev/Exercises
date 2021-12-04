838e96a17d4b1715329602d9120ec3c695534020
Alexander-Ageev <alexadrageev87@gmai.com>
2021-12-04 23:44:24 +0300

    Ex. 10

--- ASD_1/10/main.py
+++ ASD_1/10/main.py
@@ -1,113 +1,71 @@
-SET_LEN = 65000
-STEP = 11
 class PowerSet():
     def __init__(self):
-        self.length = SET_LEN
-        self.step = STEP
-        self.slots = [None] * self.length
-
-    def _hash_fun(self, value: str):
-        slot_number = (sum(bytes(value, encoding='utf-8')) * 32500) % self.length
-        return slot_number
-
-    def _seek_slot(self, value):
-        index = self._hash_fun(value)
-        hash_fun_index = index
-        step_count = 0
-        while self.slots[index] is not None and step_count < self.length:
-            if self.slots[index] == value:
-                return index
-            index += self.step
-            step_count += 1
-            if index >= self.length:
-                index = index % self.length
-            if index == hash_fun_index:
-                return None
-        if step_count == self.length:
-            return None
-        else:
-            return index
+        self.slots = []
 
     def put(self, value):
-        index = self._seek_slot(value)
-        if index is None:
-            res = None
-        else:
-            self.slots[index] = value
-            res = index
-        return res
+        """Добавляет элемент в множество. Всегда"""
+        if value not in self.slots:
+            self.slots.append(value)
+        return 0
 
     def size(self):
-        s = 0
-        for i in range(SET_LEN):
-            if self.slots[i] is not None:
-                s +=1
-        return s
+        """Возвращает размер множества"""
+        return len(self.list_value())
 
     def get(self, value):
-        index = self._hash_fun(value)
-        step_count = 0
-        hash_fun_index = index
-        while self.slots[index] != value:
-            index += self.step
-            if index >= SET_LEN:
-                index = index % SET_LEN
-            if index == hash_fun_index or step_count == SET_LEN - 1 or self.slots[index] is None:
-                return False
-        return True
+        """Возвращает True, если элемент присутствует в множестве, иначе возвращает False"""
+        return bool(value in self.slots)
 
     def remove(self, value):
-        index = self._seek_slot(value)
-        if index is not None:
-            self.slots[index] = None
-            return True
-        else:
-            return False
+        """Удаляет элемент value, возвращает True. Иначе возвращает False"""
+        for i in self.slots:
+            if value == i:
+                self.slots.remove(i)
+                return True
+        return False
 
     def list_value(self):
-        res = []
-        for i in range(SET_LEN):
-            if self.slots[i] is not None:
-                res.append(self.slots[i])
-        return res
+        """Возвращает список элементов множества"""
+        return self.slots
 
     def intersection(self, set2):
-        # пересечение текущего множества и set2
+        """Возвращает пересечение исходного множества с параметром set2"""
         res = PowerSet()
         values = self.list_value()
         for i in values:
-            if set2.get(i) == True:
-                res.put(i)
+            if set2.get(i):
+                res.slots.append(i)
         return res
 
     def union(self, set2):
+        """Возвращает объединение исходного множества с параметром set2"""
         res = PowerSet()
-        values = self.list_value() + set2.list_value()
-        for i in values:
-            res.put(i)
+        values = sorted(self.list_value() + set2.list_value())
+        prev = values[0]
+        res.put(prev)
+        for i in range(1, len(values)):
+            if prev != values[i]:
+                res.slots.append(values[i])
+            prev = values[i]
         return res
 
     def difference(self, set2):
-        # разница текущего множества и set2
+        """Возвращает отличающиеся элементы исходного множества от set2"""
         res = PowerSet()
         values = self.list_value()
         for i in values:
-            if set2.get(i) != True:
-                res.put(i)
+            if not set2.get(i):
+                res.slots.append(i)
         return res
 
     def issubset(self, set2):
-        # возвращает True, если set2 есть
-        # подмножество текущего множества,
-        # иначе False
-        a = sorted(self.list_value())
-        b = sorted(set2.list_value())
-        if a < b:
-            return False
-        else:
-            i = 0
+        """Возвращает   True, если set2 полностью входит в исходное множество"""
+        subset_size = set2.size()
+        if self.size() >= subset_size:
             count = 0
-            while b[i] in a:
-                i += 1
-                count += 1
-        return True if count == len(b) else False
+            subset = set2.list_value()
+            for i in subset:
+                if self.get(i):
+                    count += 1
+            return bool(count == subset_size)
+        return False
--- ASD_1/10/test.py
+++ ASD_1/10/test.py
@@ -1,17 +1,5 @@
-import unittest, random, cProfile
+import unittest, random
 from main import PowerSet
-
-def profile(func):
-    """Decorator for run function profile"""
-    def wrapper(*args, **kwargs):
-        profile_filename = func.__name__ + '.prof'
-        profiler = cProfile.Profile()
-        result = profiler.runcall(func, *args, **kwargs)
-        profiler.dump_stats(profile_filename)
-        return result
-    return wrapper
-
-
 
 class main_tests(unittest.TestCase):
     def test_put_new(self):
@@ -86,13 +74,12 @@ class main_tests(unittest.TestCase):
         res = sorted([])
         self.assertEqual(data, res)
 
-    @profile
     def test_intersection_random(self):
         set1 = PowerSet()
         set1_list = []
         set2 = PowerSet()
         set2_list = []
-        for i in range(20000):
+        for i in range(1000):
             a = str(random.randint(0,100000))
             b = str(random.randint(0,100000))
             set1.put(a)
@@ -105,25 +92,102 @@ class main_tests(unittest.TestCase):
         res = sorted(list(a & b))
         self.assertEqual(data, res)
 
+    def test_union_void(self):
+        set1 = PowerSet()
+        set2 = PowerSet()
+        set2.put('0')
+        set2.put(':')
+        set2.put('D')
+        data = sorted(set1.union(set2).list_value())
+        res = sorted(['0', ':', 'D'])
+        self.assertEqual(data, res)
+
     def test_union_random(self):
         set1 = PowerSet()
         set1_list = []
         set2 = PowerSet()
         set2_list = []
-        for i in range(100):
-            a = str(random.randint(0,9))
-            b = str(random.randint(0,9))
+        for i in range(1000):
+            a = str(random.randint(0,100000))
+            b = str(random.randint(0,100000))
             set1.put(a)
             set1_list.append(a)
             set2.put(b)
             set2_list.append(b)
         a = set(set1_list)
         b = set(set2_list)
-        data = sorted(set1.intersection(set2).list_value())
+        data = sorted(set1.union(set2).list_value())
         res = sorted(list(a | b))
         self.assertEqual(data, res)
 
+    def test_diffrence_void(self):
+        set1 = PowerSet()
+        set2 = PowerSet()
+        set1.put('0')
+        set1.put(':')
+        set1.put('D')
+        set2.put('0')
+        set2.put(':')
+        set2.put('D')
+        data = sorted(set1.difference(set2).list_value())
+        res = sorted([])
+        self.assertEqual(data, res)
 
+    def test_difference_random(self):
+        set1 = PowerSet()
+        set1_list = []
+        set2 = PowerSet()
+        set2_list = []
+        for i in range(1000):
+            a = str(random.randint(0,100000))
+            b = str(random.randint(0,100000))
+            set1.put(a)
+            set1_list.append(a)
+            set2.put(b)
+            set2_list.append(b)
+        a = set(set1_list)
+        b = set(set2_list)
+        data = sorted(set1.difference(set2).list_value())
+        res = sorted(list(a - b))
+        self.assertEqual(data, res)
+
+    def test_issubset_False(self):
+        set1 = PowerSet()
+        set2 = PowerSet()
+        set1.put('0')
+        set1.put(':')
+        set1.put('D')
+        set2.put('0')
+        set2.put('!')
+        set2.put('D')
+        data = set1.issubset(set2)
+        res = False
+        self.assertEqual(data, res)
+
+    def test_issubset_true(self):
+        set1 = PowerSet()
+        set2 = PowerSet()
+        set1.put('0')
+        set1.put(':')
+        set1.put('D')
+        set2.put('0')
+        set2.put('D')
+        data = set1.issubset(set2)
+        res = True
+        self.assertEqual(data, res)
+
+    def test_issubset_equal(self):
+        set1 = PowerSet()
+        set2 = PowerSet()
+        set1.put('0')
+        set1.put(':')
+        set1.put('D')
+        set2.put('0')
+        set2.put(':')
+        set2.put('D')
+        data = set1.issubset(set2)
+        res = True
+        self.assertEqual(data, res)
 
 if __name__ == '__main__':
     unittest.main()
