7dca43b94e091acb9e4de832340f3d12970f4539
Alexander Ageev <alexandrageev87@gmail.com>
2022-03-20 15:23:53 +0300

    temp

---
+++ ASD_2/2. Binary search trees/test.py
@@ -0,0 +1,11 @@
+import unittest
+from main import BSTNode, BSTFind, BST
+
+class MainTest(unittest.TestCase):
+    def setUp(self) -> None:
+        
+        return super().setUp()
+    
+
+if __name__ == "__main__":
+    unittest.main()
