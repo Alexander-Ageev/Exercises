eff0e3901c6d0bb252a165fe1e74cf5671d47149
Alexander-Ageev <alexadrageev87@gmai.com>
2022-03-22 23:49:00 +0300

    Ex. 2 don't work add method, not delete method

--- ASD_2/2. Binary search trees/main.py
+++ ASD_2/2. Binary search trees/main.py
@@ -1,5 +1,7 @@
+"""Модуль содержит реализацию типа данных БИНАРНОЕ ДЕРЕВО ПОИСКА"""
+
 class BSTNode:
-	
+    """Класс описывает узел бинарного дерева поиска"""
     def __init__(self, key, val, parent):
         self.NodeKey = key # ключ узла
         self.NodeValue = val # значение в узле
@@ -11,30 +13,65 @@ class BSTNode:
 class BSTFind: # промежуточный результат поиска
 
     def __init__(self):
+        """Создает объект с информацией о результатах поиска"""
         self.Node = None # None если 
         # в дереве вообще нету узлов
 
         self.NodeHasKey = False # True если узел найден
         self.ToLeft = False # True, если родительскому узлу надо 
         # добавить новый узел левым потомком
+    
+    def FindNode(self, current_node: BSTNode, parent_node: BSTNode, key):
+        """Поиск узла по значению, либо родительского узла, куда необходимо значение добавить"""
+        if current_node is None:
+            self.Node = parent_node
+            return self
+        if current_node.NodeKey > key:
+            self.ToLeft = True
+            return self.FindNode(current_node.LeftChild, current_node, key)
+        if current_node.NodeKey < key:
+            self.ToLeft = False
+            return self.FindNode(current_node.RightChild, current_node, key)
+        if current_node.NodeKey == key:
+            self.Node = current_node
+            self.NodeHasKey = True
+        return self
 
 class BST:
-
-    def __init__(self, node):
+    """Класс описывает структуру бинарного дереа поиска"""
+    def __init__(self, node: BSTNode):
         self.Root = node # корень дерева, или None
 
     def FindNodeByKey(self, key):
-        # ищем в дереве узел и сопутствующую информацию по ключу
-        return None # возвращает BSTFind
+        """Возвращает ссылку на узел и сопутстующую информацтю поиска"""
+        res = BSTFind()
+        return res.FindNode(self.Root, self.Root.Parent, key)
 
     def AddKeyValue(self, key, val):
-        # добавляем ключ-значение в дерево
-        return False # если ключ уже есть
+        res = self.FindNodeByKey(key) # добавляем ключ-значение в дерево
+        if res.NodeHasKey == True:
+            return False # если ключ уже есть
+        if res.Node is None:
+            self.Root = BSTNode(key, val, None)        
+        if res.ToLeft == True:
+            res.Node.LeftChild = BSTNode(key, val, res.Node)
+        if res.ToLeft == False:
+            res.Node.RightChild = BSTNode(key, val, res.Node)
+        return True
   
-    def FinMinMax(self, FromNode, FindMax):
-        # ищем максимальный/минимальный ключ в поддереве
-        # возвращается объект типа BSTNode
-        return None
+    def FinMinMax(self, FromNode: BSTNode, FindMax: bool):
+        """
+        Возвращает ссылку на узел с максимальным/минимальным значением ключа
+        FindMax = True - поиск максимума
+        FindMax = False - поиск минимума
+        """
+        if FindMax == True: # Поиск максимума
+            if FromNode.RightChild is not None:
+                return self.FinMinMax(FromNode.RightChild, FindMax)
+            return FromNode
+        if FromNode.LeftChild is not None: # Поиск минимума
+            return self.FinMinMax(FromNode.LeftChild, FindMax)
+        return FromNode
 	
     def DeleteNodeByKey(self, key):
         # удаляем узел по ключу
--- ASD_2/2. Binary search trees/test.py
+++ ASD_2/2. Binary search trees/test.py
@@ -1,11 +1,87 @@
 import unittest
-from main import BSTNode, BSTFind, BST
+from main import BSTNode, BST
 
 class MainTest(unittest.TestCase):
     def setUp(self) -> None:
-        
-        return super().setUp()
+        """Автоматическая генерация тестового дерева. Проверка метода AddKeyValue"""
+        nodes = list(range(1, 16))
+        root = BSTNode(8, 8, None)
+        tree = BST(root)
+        for i in nodes:
+            tree.AddKeyValue(i, i)
+        self.root = root
+        self.tree = tree
+
+    def test_tree(self):
+        res = []
+        for i in range(1, 16):
+            node = self.tree.FindNodeByKey(i).Node
+            if node.LeftChild is not None:
+                res.append(node.LeftChild.NodeValue)
+            else:
+                res.append(None)
+            if node.RightChild is not None:
+                res.append(node.RightChild.NodeValue)
+            else:
+                res.append(None)
+        data = [None, None, 1, 3, None, None, 2, 6, None, None, 5, 7, None, None, 4, 12, None, None,
+        9, 11, None, None, 10, 14, None, None, 13, 15, None, None]
+        self.assertEqual(data, res)
+
+    def test_find_exist_key(self):
+        """Проверка поиска существующего ключа"""
+        res = self.tree.FindNodeByKey(7).Node.Parent.NodeValue
+        data = 6
+        self.assertEqual(data, res)
+
+    def test_find_add_to_left(self):
+        """Проверка поиска для добавления узла слева"""
+        node_find = self.tree.FindNodeByKey(-1)
+        res = [node_find.Node.NodeValue, node_find.ToLeft]
+        data = [1, True]
+        self.assertEqual(data, res)
+
+    def test_find_add_to_right(self):
+        """Проверка поиска для добавления узла справа"""
+        node_find = self.tree.FindNodeByKey(18)
+        res = [node_find.Node.NodeValue, node_find.ToLeft]
+        data = [15, False]
+        self.assertEqual(data, res)
     
+    def test_add_exist_key(self):
+        """Проверка добавления ключа с существующим значением"""
+        res = self.tree.AddKeyValue(7, 7)
+        data = False
+        self.assertEqual(res, data)
+
+    def test_find_max_root(self):
+        """Поиск максимального значения начиная с корня"""
+        node = self.tree.FinMinMax(self.root, True)
+        res = node.NodeValue
+        data = 15
+        self.assertEqual(res, data)
+
+    def test_find_min_root(self):
+        """Поиск минимального значения начиная с корня"""
+        node = self.tree.FinMinMax(self.root, False)
+        res = node.NodeValue
+        data = 1
+        self.assertEqual(res, data)
+
+    def test_find_max_node(self):
+        """Поиск максимального значения начиная с NodeValue = 4"""
+        node = self.tree.FindNodeByKey(4).Node
+        res = self.tree.FinMinMax(node, True).NodeValue
+        data = 7
+        self.assertEqual(res, data)
+
+    def test_find_min_node(self):
+        """Поиск минимального значения начиная с NodeValue = 10"""
+        node = self.tree.FindNodeByKey(10).Node
+        print(node.LeftChild)
+        res = self.tree.FinMinMax(node, False).NodeValue
+        data = 9
+        self.assertEqual(res, data)
 
 if __name__ == "__main__":
     unittest.main()
