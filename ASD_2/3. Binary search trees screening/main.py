3ba00ab9bde5699ff6699e882119057325fd7f45
Alexander Ageev <alexandrageev87@gmail.com>
2022-03-29 20:31:19 +0300

    Ex.3. Refactoring nested conditions

--- ASD_2/3. Binary search trees screening/main.py
+++ ASD_2/3. Binary search trees screening/main.py
@@ -72,11 +72,11 @@ class BST:
         FindMax = True - поиск максимума
         FindMax = False - поиск минимума
         """
-        if FindMax is True:
-            if FromNode.RightChild is not None:
-                return self.FinMinMax(FromNode.RightChild, FindMax)
+        if FindMax is True and FromNode.RightChild is not None:
+            return self.FinMinMax(FromNode.RightChild, FindMax)
+        elif FindMax is True and FromNode.RightChild is None:
             return FromNode
-        if FromNode.LeftChild is not None:
+        elif FindMax is False and FromNode.LeftChild is not None:
             return self.FinMinMax(FromNode.LeftChild, FindMax)
         return FromNode
 
@@ -217,21 +217,21 @@ class BST:
         """
         res = []
         for i in nodes:
-            if not detail:
-                data = i.NodeValue
+            if i.Parent is None:
+                parent = None
             else:
-                if i.Parent is None:
-                    parent = None
-                else:
-                    parent = i.Parent.NodeValue
-                if i.LeftChild is None:
-                    left_child = None
-                else:
-                    left_child = i.LeftChild.NodeValue
-                if i.RightChild is None:
-                    right_child = None
-                else:
-                    right_child = i.RightChild.NodeValue
+                parent = i.Parent.NodeValue
+            if i.LeftChild is None:
+                left_child = None
+            else:
+                left_child = i.LeftChild.NodeValue
+            if i.RightChild is None:
+                right_child = None
+            else:
+                right_child = i.RightChild.NodeValue
+            if detail:
                 data = (parent, i.NodeValue, left_child, right_child)
+            else:        
+                data = i.NodeValue
             res.append(data)
         return res
