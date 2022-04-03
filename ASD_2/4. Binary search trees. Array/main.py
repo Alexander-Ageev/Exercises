84486bf4c34b8f9eb3e948762c455934924d598e
Alexander-Ageev <alexadrageev87@gmai.com>
2022-04-03 22:24:03 +0300

    Ex. 4

--- ASD_2/4. Binary search trees. Array/main.py
+++ ASD_2/4. Binary search trees. Array/main.py
@@ -2,6 +2,7 @@
 Модуль содержит реализацию бинарного дерева поиска через массив ключей.
 Данная реализация расчитана только на добавление новых узлов (удаление узлов и ветвей невозможно)
 """
+
 class aBST:
     def __init__(self, depth):
         """
@@ -11,41 +12,49 @@ class aBST:
         """
         tree_size = 0
         for i in range(depth):
-            tree_size += 2 ** depth
+            tree_size += 2 ** i
         self.tree_size = tree_size
         self.Tree = [None] * tree_size # массив ключей
-	
+
     def FindKeyIndex(self, key):
-        """Возвращает индекс ключа, если ключ не найден - возвращает None."""
-        search_result = self._FindKey_(0, key)
-        if search_result[1] is True:
-            index = search_result[0]
-        else:
-            index = None
+        """Инициализация _FindKey_"""
+        index = self._FindKey_(0, key)
         return index
-	
+
     def AddKey(self, key):
-        search_result = self._FindKey_(0, key)
-        index = search_result[0]
-        empty_node = not search_result[1]
-        if empty_node is True:
+        """
+        Возвращает индекс добавленного ключа.
+        Если нет свободного слота для нового ключа, возвращает -1
+        """
+        index = self._FindKey_(0, key)
+        if index is None:
+            index = -1
+        elif index == 0 and self.Tree[index] is None:
             self.Tree[index] = key
-        else:
-            pass
-            #TODO: добавить метод resize array
-            #TODO: добавить ключ по новому адресу
-        return -1; 
-        # индекс добавленного/существующего ключа или -1 если не удалось
+        elif index == 0 and self.Tree[index] is not None:
+            index = -1
+        elif index < 0:
+            index = abs(index)
+            self.Tree[index] = key
+        return index
 
     def _FindKey_(self, current_index, key):
-        if current_index >= self.tree_size - 1:
-            current_index = (current_index-1) // 2
-            return (current_index, False)
-        if self.Tree[current_index] == key:
-            return (current_index, True)
+        """
+        Возвращает индекс узла в массиве, если ключ найден.
+        Если ключ не найден, возвращает индекс подходящей свободной
+        ячейки в массиве.
+        Если свободной ячейки нет, возвращает None
+        """
+        if current_index >= self.tree_size:
+            index = None
+        elif self.Tree[current_index] is None:
+            index = -1 * current_index
+        elif self.Tree[current_index] == key:
+            index = current_index
         elif key < self.Tree[current_index]:
             next_index = current_index * 2 + 1
-            return self._FindKey_(next_index, key)
+            index = self._FindKey_(next_index, key)
         elif key > self.Tree[current_index]:
             next_index = current_index * 2 + 2
-            return self._FindKey_(next_index, key)
+            index = self._FindKey_(next_index, key)
+        return index
--- ASD_2/4. Binary search trees. Array/test.py
+++ ASD_2/4. Binary search trees. Array/test.py
@@ -1,14 +1,63 @@
+"""Тесты для модуля, реализующего бинарное дерево поиска через массив"""
 import unittest
-import random
 from main import aBST
+
+ROOT = 50
+TEST_NODES = [25, 75, 37, 62, 84, 31, 43, 55, 92]
 
 class MainTest(unittest.TestCase):
     def setUp(self) -> None:
+        """Создание тестового дерева"""
+        self.tree = aBST(4)
+        self.tree.AddKey(ROOT)
+        for i in TEST_NODES:
+            self.tree.AddKey(i)
 
+    def test_tree_setup(self):
+        """Проверка корректности создания тестового дерева"""
+        res = (self.tree.tree_size, self.tree.Tree)
+        data = (15, [50, 25, 75, None, 37, 62, 84, None, None, 31, 43, 55, None, None, 92])
+        self.assertEqual(res, data)
 
+    def test_no_empty_slot(self):
+        """Проверка на невозможность добавить новый ключ"""
+        res = self.tree.FindKeyIndex(98)
+        data = None
+        self.assertEqual(res, data)
 
-    def test(self):
-        pass
+    def test_find_empty_slot_key(self):
+        """Проверка на корректность поиска свободного слота"""
+        res = self.tree.FindKeyIndex(22)
+        data = -3
+        self.assertEqual(data, res)
+
+    def test_zero_index_key_equal(self):
+        """Проверка на корректность работы метода FindKeyIndex. Все дерево занято, ключ найден"""
+        tree = aBST(1)
+        tree.AddKey(ROOT)
+        res = tree.FindKeyIndex(50)
+        data = 0
+        self.assertEqual(data, res)
+
+    def test_zero_index_key_not_equal(self):
+        """Проверка на корректность работы метода FindKeyIndex. Все дерево занято, ключ не найден"""
+        tree = aBST(1)
+        tree.AddKey(ROOT)
+        res = tree.FindKeyIndex(51)
+        data = None
+        self.assertEqual(data, res)
+
+    def test_full_tree(self):
+        """Заплняем дерево, проверяем результат"""
+        new_nodes = [22, 6, 76, 63, 59, 23]
+        new_indexes = []
+        for i in new_nodes:
+            new_indexes.append(self.tree.AddKey(i))
+        res = (self.tree.tree_size, self.tree.Tree, new_indexes)
+        data = (15,
+                [50, 25, 75, 22, 37, 62, 84, 6, 23, 31, 43, 55, 63, 76, 92],
+                [3, 7, 13, 12, -1, 8])
+        self.assertEqual(res, data)
 
 if __name__ == "__main__":
     unittest.main()
