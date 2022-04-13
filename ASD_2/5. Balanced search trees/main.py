1807fecebdac902763a5402866b3e804d7a8e9a1
Alexander-Ageev <alexadrageev87@gmai.com>
2022-04-13 20:16:25 +0300

    Ex.5

--- ASD_2/5. Balanced search trees/main.py
+++ ASD_2/5. Balanced search trees/main.py
@@ -1,6 +1,7 @@
-"""Модуль описывает метод создания сбалансированного дерева поиска"""
+"""Модуль описывает функцию балансировки дерева поиска"""
 
 def GetMiddleIndex(array: list):
+    "Возвращает индекс середины массива. Если массив пустой - возвращает -1"
     size = len(array)
     if size == 0:
         mid_index = -1
@@ -8,38 +9,25 @@ def GetMiddleIndex(array: list):
         mid_index = size // 2
     return mid_index
 
-def GenArray(array: list, left: list, right: list):
-    left_child_index = GetMiddleIndex(left)
-    if left_child_index != -1:
-        array.append(left[left_child_index])
-        GenArray(array, left[0: left_child_index], left[left_child_index+1:])
-    
-    right_child_index = GetMiddleIndex(right)
-    if right_child_index != -1:
-        array.append(right[right_child_index])
-        GenArray(array, right[0: right_child_index], right[right_child_index+1:])
-    return array
-
-"""
-def GenArray(array: list, left: list, right: list):
-    left_child_index = GetMiddleIndex(left)
-    if left_child_index == -1:
-        return array
-    right_child_index = GetMiddleIndex(right)
-    if right_child_index == -1:
-        return array
-    array.append(left[left_child_index])
-    array.append(right[right_child_index])
-    GenArray(array, left[0: left_child_index], left[left_child_index+1:])
-    GenArray(array, right[0: right_child_index], right[right_child_index+1:])
-    return array
-"""
+def GenArray(roots: list, parts: list):
+    """
+    Получает на вход отсортированный по возрастанию массив.
+    Возвращает массив, описывающий структуру сбалансированного дерева
+    """
+    new_parts = []
+    for i in parts:
+        mid = GetMiddleIndex(i)
+        if mid != -1:
+            left_branch = i[0: mid]
+            right_branch = i[mid+1:]
+            roots.append(i[mid])
+            new_parts += [left_branch, right_branch]
+    if new_parts != []:
+        GenArray(roots, new_parts)
+    return roots
 
 def GenerateBBSTArray(a: list):
+    """Инициализация GenArray"""
     a.sort()
-    root_index = GetMiddleIndex(a)
-    root = a[root_index]
-    left = a[0: root_index]
-    right = a[root_index+1:]
-    array = GenArray([root], left, right)
+    array = GenArray([], [a])
     return array
--- ASD_2/5. Balanced search trees/test.py
+++ ASD_2/5. Balanced search trees/test.py
@@ -1,14 +1,36 @@
+"""Тесты для функции балансировки бинарного дерева поиска"""
 import unittest
 import random
 from main import GenerateBBSTArray
 
 class MainTest(unittest.TestCase):
     def test_tree(self):
+        """Проверка несортированным тестовым деревом"""
         array = [i+1 for i in range(15)]
         random.shuffle(array)
-        print(array)
         res = GenerateBBSTArray(array)
         data = [8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15]
+        self.assertEqual(res, data)
+
+    def test_one_element_tree(self):
+        """Проверка деревом, состощим из корня"""
+        array = [1]
+        res = GenerateBBSTArray(array)
+        data = [1]
+        self.assertEqual(res, data)
+
+    def test_zero_tree(self):
+        """Проверка пустым деревом"""
+        array = []
+        res = GenerateBBSTArray(array)
+        data = []
+        self.assertEqual(res, data)
+
+    def test_not_full_tree(self):
+        """Проверка неполным деревом"""
+        array = [1, 2, 4, 5, 6]
+        res = GenerateBBSTArray(array)
+        data = [4, 2, 6, 1, 5]
         self.assertEqual(res, data)
 
 if __name__ == '__main__':
