416f2473097c6ccea9a4ffc1bc4035f7786b4dc0
Alexander Ageev <alexandrageev87@gmail.com>
2022-04-27 00:11:57 +0300

    Ex.7. Temp

--- ASD_2/7. Heap_array/main.py
+++ ASD_2/7. Heap_array/main.py
@@ -1,27 +1,41 @@
 """
 Модуль описывает реализацию типа данных Пирамида через массив.
 """
+
 class Heap:
 
     def __init__(self):
         self.HeapArray = [] # хранит неотрицательные числа-ключи
-        self.max_size = 0
-		
+        self.MaxSize = 0
+	
     def MakeHeap(self, a, depth):
         """
         Создает пирамиду из массива a.
         Максимальный размер массива определяется величиной depth.
         """
-        self.max_size = 2 ** (depth+1) - 1
-        if len(a) > self.max_size:
-            a = a[0: self.max_size]
+        self.MaxSize = 2 ** (depth+1) - 1
+        if len(a) > self.MaxSize:
+            a = a[0: self.MaxSize]
         for element in a:
-            print('el:', element)
             self.Add(element)   
 
     def GetMax(self):
-        # вернуть значение корня и перестроить кучу
-        return -1 # если куча пуста
+        """
+        Возвращает значение корня и перестраивает пирамиду.
+        Возвращает -1, если пирамида пуста.
+        """
+        if len(self.HeapArray) == 0:
+            return -1
+        elif len(self.HeapArray) == 1:    
+            root = self.HeapArray.pop(0)
+        else:
+            self.HeapArray[0], self.HeapArray[-1] = self.HeapArray[-1], self.HeapArray[0]
+            root = self.HeapArray.pop(-1)
+            self.MoveDown(self.HeapArray[0], 0)
+        
+        print('root:', root)
+
+        return root
 
     def Add(self, key):
         """
@@ -29,15 +43,15 @@ class Heap:
         Возвращает True при успешном добавлении элемента.
         Возвращает False, если пирамида заполнена.
         """
-        if len(self.HeapArray) >= self.max_size:
+        if len(self.HeapArray) >= self.MaxSize:
             return False
         self.HeapArray.append(key)
         self.MoveUp(key, len(self.HeapArray)-1)
         return True
-    
-    def GetHeap(self):
-        return self.HeapArray
 
+    def GetHeap(self):
+        """Возвращает массив ключей пирамиды"""
+        return self.HeapArray
 
     def MoveUp(self, key, index):
         """
@@ -49,6 +63,22 @@ class Heap:
             self.HeapArray[index], self.HeapArray[parent_index] = self.HeapArray[parent_index], key
             self.MoveUp(key, parent_index)
 
+    def MoveDown(self, key, index):
+        """
+        Перестраивает пирамиду просеивая сверху вниз:
+        если родитель меньше максимального из потомков -> родитель и потомок меняются местами.
+        Операция повторяется, пока родитель не будет больше обоих своих потомков.
+        """
+        
+        print('array:', self.HeapArray)
+        
+        left_child_index = 2 * index + 1
+        childs = self.HeapArray[left_child_index: left_child_index + 2]
+        max_child = max(childs)
+        max_index = left_child_index + childs.index(max_child)
 
-    def MoveDown(self):
-        pass
+        print("max_index:", max_index)
+
+        if max_index < len(self.HeapArray) and key < max_child:
+            self.HeapArray[index], self.HeapArray[max_index] = self.HeapArray[max_index], self.HeapArray[index]
+            self.MoveDown(key, max_index)
--- ASD_2/7. Heap_array/test.py
+++ ASD_2/7. Heap_array/test.py
@@ -2,13 +2,83 @@ import unittest
 from main import Heap
 
 class MainTest(unittest.TestCase):
-    def test_add_array(self):
+    def test_make_simple_heap(self):
+        """Проверка на создание простой пирамиды"""
         heap = Heap()
         array = [1, 2, 3]
         heap.MakeHeap(array, 1)
         data = heap.GetHeap()
         res = [3, 1, 2]
         self.assertEqual(data, res)
+
+    def test_make_oversize_heap(self):
+        """Проверка на создание пирамиды с излишними входными данными"""
+        heap = Heap()
+        array = [1, 2, 3, 5, 7, 9]
+        heap.MakeHeap(array, 1)
+        data = heap.GetHeap()
+        res = [3, 1, 2]
+        self.assertEqual(data, res)
+
+    def test_make_lowsize_heap(self):
+        """Проверка на создание пирамиды со свободными ячейками"""
+        heap = Heap()
+        array = [1, 2, 3]
+        heap.MakeHeap(array, 4)
+        data = heap.GetHeap()
+        res = [3, 1, 2]
+        self.assertEqual(data, res)
+
+    def test_make_empty_heap(self):
+        """Проверка на создание пустой пирамиды"""
+        heap = Heap()
+        array = []
+        heap.MakeHeap(array, 4)
+        data = heap.GetHeap()
+        res = []
+        self.assertEqual(data, res)
+    
+    def test_add_duplicate_element(self):
+        """Проверка на добавление существующего элемента"""
+        heap = Heap()
+        array = [1, 2, 3]
+        heap.MakeHeap(array, 2)
+        heap.Add(3)
+        data = heap.GetHeap()
+        res = [3, 3, 2, 1]
+        self.assertEqual(data, res)
+
+    def test_add_in_full_heap(self):
+        """Проверка на создание простой пирамиды"""
+        heap = Heap()
+        array = [1, 2, 3]
+        heap.MakeHeap(array, 1)
+        add_ok = heap.Add(5)
+        data = (add_ok, heap.GetHeap())
+        res = (False, [3, 1, 2])
+        self.assertEqual(data, res)
+    
+    def test_get_max(self):
+        """Проверка на удаление корня"""
+        heap = Heap()
+        array = [1, 2, 3]
+        heap.MakeHeap(array, 1)
+        data = (heap.GetMax(), heap.GetHeap())
+        res = (3, [2, 1])
+        self.assertEqual(data, res)
+
+    def test_get_all(self):
+        """Проверка на удаление всех элементов"""
+        heap = Heap()
+        array = [1, 2, 3, 5, 4]
+        heap.MakeHeap(array, 3)
+        max_array = []
+        for i in range(len(array)+1):
+            max_array.append(heap.GetMax())
+        data = (max_array, heap.GetHeap())
+        res = ([5, 4, 3, 2, 1, -1], [])
+        self.assertEqual(data, res)
+
 
 if __name__ == '__main__':
     unittest.main()
